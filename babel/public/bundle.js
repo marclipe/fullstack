53376afcb47369e90d87d2450949e0546786ac1b
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2022-10-27 23:54:04 -0300

    Adicionando o Babel e dependências

--- babel/public/bundle.js
+++ babel/public/bundle.js
@@ -1,17 +1,6 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-var Person = /*#__PURE__*/function () {
-  function Person() {
-    _classCallCheck(this, Person);
-  }
-  _createClass(Person, [{
-    key: "run",
-    value: function run() {
-      console.log('hello');
-    }
-  }]);
-  return Person;
-}();
+var numbers = [1, 2, 3, 4];
+numbers.forEach(function (number, index) {
+  console.log(number, index);
+});
