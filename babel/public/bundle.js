0f7fac8eb1a51155f1ab40ce0479210f096d227d
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2022-10-31 22:21:39 -0300

    Map

--- babel/public/bundle.js
+++ babel/public/bundle.js
@@ -1,6 +1,8 @@
 "use strict";
 
-var numbers = [1, 2, 3, 4];
-numbers.forEach(function (number, index) {
-  console.log(number, index);
+var numbers = [1, 2, 3, 4, 5];
+var newNumbers = numbers.map(function (number) {
+  return number + 1;
 });
+console.log(newNumbers);
+console.log(numbers);
