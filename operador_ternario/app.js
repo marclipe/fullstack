42cea8d91353b3dfb863b938a23c8e242f4029f8
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2022-10-31 22:43:34 -0300

    Operador Ternário

---
+++ operador_ternario/app.js
@@ -0,0 +1,11 @@
+var age = 18
+var result = age >= 18 ? 'ok' : 'não ok'
+
+console.log(result)
+
+//--------------
+
+var age = 17
+var result
+result = age >= 18 ? 'Yes!!' : 'No :('
+console.log(`I can drive? ${result}`) 
